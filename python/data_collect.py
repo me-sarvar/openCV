409f401d20ad1bd89ba0289a5dfdbf44462445d4
Sarvar Juraev <115955125+swesarvar@users.noreply.github.com>
2023-05-26 01:36:28 +0900

    data_set folder is created

---
+++ python/data_collect.py
@@ -0,0 +1,86 @@
+import os
+import cv2
+import numpy as np
+import mediapipe as mp
+
+
+mp_holistic = mp.solutions.holistic
+mp_drawing = mp.solutions.drawing_utils
+
+
+def mediapipe_detection(image, model):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image.flags.writeable = False
+    results = model.process(image)
+    image.flags.writeable = True
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    return image, results
+
+
+def draw_landmarks(image, results):
+    mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACEMESH_CONTOURS)
+    mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS)
+    mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
+    mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
+
+
+def draw_styled_landmarks(image, results):
+    # Draw face connections
+    mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACEMESH_CONTOURS,
+                              mp_drawing.DrawingSpec(color=(0, 0, 255), thickness=1, circle_radius=1),
+                              mp_drawing.DrawingSpec(color=(255, 255, 0), thickness=1, circle_radius=1)
+                              )
+    # Draw pose connections
+    mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
+                              mp_drawing.DrawingSpec(color=(0, 0, 255), thickness=2, circle_radius=4),
+                              mp_drawing.DrawingSpec(color=(255, 255, 0), thickness=2, circle_radius=2)
+                              )
+    # Draw left hand connections
+    mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
+                              mp_drawing.DrawingSpec(color=(255, 0, 255), thickness=3, circle_radius=3),
+                              mp_drawing.DrawingSpec(color=(0, 255, 255), thickness=3, circle_radius=3)
+                              )
+    # Draw right hand connections
+    mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
+                              mp_drawing.DrawingSpec(color=(0, 128, 128), thickness=3, circle_radius=3),
+                              mp_drawing.DrawingSpec(color=(128, 0, 128), thickness=3, circle_radius=3)
+                              )
+    
+
+def extract_keypoints(results):
+    pose = np.array([[res.x, res.y, res.z, res.visibility] for res in results.pose_landmarks.landmark]).flatten() if results.pose_landmarks else np.zeros(132)
+    face = np.array([[res.x, res.y, res.z] for res in results.face_landmarks.landmark]).flatten() if results.face_landmarks else np.zeros(1404)
+    lh = np.array([[res.x, res.y, res.z] for res in results.left_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else np.zeros(63)
+    rh = np.array([[res.x, res.y, res.z] for res in results.right_hand_landmarks.landmark]).flatten() if results.right_hand_landmarks else np.zeros(63)
+    return np.concatenate([pose, face, lh, rh])
+
+
+DATA_SET_PATH = os.path.join('data_set')
+actions = np.array(['hello', 'thanks', 'iloveyou'])
+no_sequences = 50
+sequence_length = 30
+
+for action in actions:
+    for sequence in range(no_sequences):
+        try:
+            os.makedirs(os.path.join(DATA_SET_PATH, action, str(sequence)))
+        except:
+            pass
+
+
+def get_data():
+    cap = cv2.VideoCapture(1)
+    with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
+        while cap.isOpened():
+            ret, frame = cap.read()
+            image, results = mediapipe_detection(frame, holistic)
+            print(results)
+            draw_styled_landmarks(image, results)
+            if cv2.waitKey(10) & 0xFF == ord('q'):
+                break
+            cv2.imshow('Data Collection', image)
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+get_data()
